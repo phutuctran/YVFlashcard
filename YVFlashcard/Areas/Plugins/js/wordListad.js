62c8dd70b90c1ee57b4ee619a18d1ed55779cd2a
thaiphungng <thaiphung.ng@gmail.com>
2023-12-20 15:37:41 +0700

    update fe

--- YVFlashcard/Areas/Plugins/js/wordListad.js
+++ YVFlashcard/Areas/Plugins/js/wordListad.js
@@ -41,3 +41,70 @@
     }, {offset: '80%'});
 
 })(jQuery);
+
+function loadCreateImage() {
+    var fileInput = document.getElementById('InputImg');
+    var createImg = document.getElementById('createImg');
+
+    // Check if a file is selected
+    if (fileInput.files && fileInput.files[0]) {
+        var reader = new FileReader();
+
+        // Set up the FileReader to read the selected file
+        reader.onload = function (e) {
+            // Set the source of the createImg to the loaded image
+            createImg.src = e.target.result;
+        };
+
+        // Read the selected file as a data URL
+        reader.readAsDataURL(fileInput.files[0]);
+    }
+}
+
+function loadImage() {
+    var fileInput = document.getElementById('fileInput');
+    var modalImg = document.getElementById('modalImg');
+
+    // Check if a file is selected
+    if (fileInput.files && fileInput.files[0]) {
+        var reader = new FileReader();
+
+        // Set up the FileReader to read the selected file
+        reader.onload = function (e) {
+            // Set the source of the modalImg to the loaded image
+            modalImg.src = e.target.result;
+        };
+
+        // Read the selected file as a data URL
+        reader.readAsDataURL(fileInput.files[0]);
+    }
+}
+
+function saveLesson() {
+    let lessonName = document.getElementById("lessonName").value;
+}
+
+function saveAddVocab() {
+    let imgSrc = document.getElementById("InputImg").src;
+    let vocab = document.getElementById("createVocab").value;
+    let pro = document.getElementById("createPro").value;
+    let part = document.getElementById("createPart").value;
+    let def = document.getElementById("createDef").value;
+}
+
+function saveEditVocab() {
+    let imgSrc = document.getElementById("modalImg").src;
+    let vocab = document.getElementById("editVocab").value;
+    let pro = document.getElementById("editPro").value;
+    let part = document.getElementById("editPart").value;
+    let def = document.getElementById("editDef").value;
+}
+
+
+function editHandle(buttonId) {
+    const imgModal = document.getElementById("modalImg");
+    let vocab = document.getElementById("editVocab").value;
+    let pro = document.getElementById("editPro").value;
+    let part = document.getElementById("editPart").value;
+    let def = document.getElementById("editDef").value;
+}
