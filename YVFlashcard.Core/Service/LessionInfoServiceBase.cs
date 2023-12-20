04b36b6a31ab88ff71dd51b2a42bdd7db373799d
Phú Túc <102684682+phutuctran@users.noreply.github.com>
2023-12-20 15:38:19 +0700

    update service

---
+++ YVFlashcard.Core/Service/LessionInfoServiceBase.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YVFlashcard.Core.DTO;
+using YVFlashcard.Core.Service.Interface;
+
+namespace YVFlashcard.Core.Service
+{
+    internal class LessionInfoServiceBase : IServiceBase<LessionInfoDTO, int>
+    {
+        public void DeleteById(int key, string userSession = null)
+        {
+            using (var context = new YVFlashCardEntities())
+            {
+                using (var trans = context.Database.BeginTransaction())
+                {
+                    LessionInfo lessionInfo = context.LessionInfo.FirstOrDefault(x => x.themeId == key);
+                    //context.LessionWord.RemoveRange(lessionInfo.LessionWord);
+                    //context.LessionWord.RemoveRange(lessionInfo.StudyHistory);
+                    context.LessionInfo.Remove(lessionInfo);
+                    context.SaveChanges();
+                    trans.Commit();
+                }
+            }
+        }
+
+        public List<LessionInfoDTO> GetAll()
+        {
+            using (var context = new YVFlashCardEntities())
+            {
+                return context.LessionInfo
+                    .Select(x => new LessionInfoDTO()
+                    {
+                        lessionInfoId = x.lessionInfoId,
+                        themeId = x.themeId,
+                        name = x.name
+                        
+                    })
+                    .ToList();
+            }
+        }
+
+        public LessionInfoDTO GetById(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LessionInfoDTO Insert(LessionInfoDTO entity)
+        {
+            using (var context = new YVFlashCardEntities())
+            {
+                LessionInfo lessionInfo = new LessionInfo()
+                {
+                    name = entity.name,
+                    description = entity.description,
+                    themeId = entity.themeId
+                };
+                context.LessionInfo .Add(lessionInfo);
+                context.SaveChanges();
+                return entity;
+            }
+        }
+
+        public void Update(int key, LessionInfoDTO entity)
+        {
+            using (var context = new YVFlashCardEntities())
+            {
+                using (var trans = context.Database.BeginTransaction())
+                {
+                    LessionInfo entityInfo = context.LessionInfo.FirstOrDefault(x => x.lessionInfoId == key);
+                    entityInfo.name = entity.name;
+                    entityInfo.description = entity.description;
+                    context.SaveChanges();
+                    trans.Commit();
+                }
+            }
+        }
+    }
+}
---
+++ YVFlashcard.Core/Service/WordServiceBase.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YVFlashcard.Core.DTO;
+using YVFlashcard.Core.Service.Interface;
+
+namespace YVFlashcard.Core.Service
+{
+    internal class WordServiceBase : IServiceBase<WordDTO, int>
+    {
+      
+            public void DeleteById(int key, string userSession = null)
+            {
+                using (var context = new YVFlashCardEntities())
+                {
+                    using (var trans = context.Database.BeginTransaction())
+                    {
+                       Word word = context.Word.FirstOrDefault(x => x.wordId == key);
+                        //context.LessionWord.RemoveRange(lessionInfo.LessionWord);
+                        //context.LessionWord.RemoveRange(lessionInfo.StudyHistory);
+                        context.Word.Remove(word);
+                        context.SaveChanges();
+                        trans.Commit();
+                    }
+                }
+            }
+
+            public List<WordDTO> GetAll()
+            {
+                using (var context = new YVFlashCardEntities())
+                {
+                    return context.Word
+                        .Select(x => new WordDTO()
+                        {
+                            wordId = x.wordId,
+                            word1 = x.word1,    
+                            pronunciation = x.pronunciation,
+                            definition = x.definition,
+                            partOfSpeech = x.partOfSpeech,
+                            imageOrSynomyn = x.imageOrSynomyn,
+
+                        })
+                        .ToList();
+                }
+            }
+
+            public WordDTO GetById(int key)
+            {
+                using (var context = new YVFlashCardEntities())
+                {
+                    return context.Word
+                        .Where(x => x.wordId == key)
+                        .Select(x => new WordDTO()
+                        {
+                            wordId = x.wordId,
+                            word1 = x.word1,
+                            pronunciation = x.pronunciation,
+                            definition = x.definition,
+                            partOfSpeech = x.partOfSpeech,
+                            imageOrSynomyn = x.imageOrSynomyn,
+
+                        })
+                        .FirstOrDefault();
+                }
+            }
+
+            public WordDTO Insert(WordDTO x)
+            {
+                using (var context = new YVFlashCardEntities())
+                {
+                Word word = new Word()
+                {
+                    word1 = x.word1,
+                    pronunciation = x.pronunciation,
+                    definition = x.definition,
+                    partOfSpeech = x.partOfSpeech,
+                    imageOrSynomyn = x.imageOrSynomyn
+                };
+                    
+                    context.Word.Add(word);
+                    context.SaveChanges();
+                return x;
+                }
+            }
+
+            public void Update(int key, WordDTO entity)
+            {
+                using (var context = new YVFlashCardEntities())
+                {
+                    using (var trans = context.Database.BeginTransaction())
+                    {
+                        Word word = context.Word.FirstOrDefault(x => x.wordId== key);
+                        word.pronunciation = entity.pronunciation;
+                        word.definition = entity.definition;
+                        word.partOfSpeech = entity.partOfSpeech;
+                        word.imageOrSynomyn = entity.imageOrSynomyn;
+                        context.SaveChanges();
+                        trans.Commit();
+                    }
+                }
+            }
+        }
+}
+
