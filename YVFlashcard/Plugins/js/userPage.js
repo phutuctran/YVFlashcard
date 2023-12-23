239784028c085cf032b34fa194e453339459b8ed
thaiphungng <thaiphung.ng@gmail.com>
2023-12-24 01:02:02 +0700

    update user page

--- YVFlashcard/Plugins/js/userPage.js
+++ YVFlashcard/Plugins/js/userPage.js
@@ -1,3 +1,28 @@
-function clicker(){
-    document.getElementById('file-btn').click();
+function loadAvtImage() {
+    var finder = new CKFinder();
+    finder.selectActionFunction = function (fileUrl) {
+
+        document.getElementById("AvtImg").src = fileUrl;
+        document.getElementById("inpAvtImage").value = fileUrl;
     }
+    finder.popup();
+}
+
+function doubleCheckNewPass() {
+    let newPass = document.getElementById("newpass-input").value;
+    let confirPass = document.getElementById("confirm-input").value;
+    if (newPass === confirPass)
+        return true;
+    else
+        return false;
+}
+let avtImg = document.getElementById("AvtImg").src;
+let iptImg = document.getElementById("inpAvtImage").value;
+let userName = document.getElementById("userName-input").value;
+let email = document.getElementById("emailUser-input").value;
+let curPass = document.getElementById("pass-input").value;
+let newPass = document.getElementById("newpass-input").value;
+let confirPass = document.getElementById("confirm-input").value;
+
+
+
